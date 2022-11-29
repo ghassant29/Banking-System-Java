f2be4ae035144ea8774d99e46bc6c51d9d983a6f
Ghassan El Turk <gass304@gmail.com>
2022-11-29 15:26:44 +0200

    Implementing switch case of show menu method

--- BankingSystemProject.java
+++ BankingSystemProject.java
@@ -59,7 +59,61 @@ public class BankingSystemProject {
 		System.out.println("C : Withdraw");
 		System.out.println("D : Previous Transaction");
 		System.out.println("E : Exit The System");
-        
+        do {
+			System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+			System.out.println("Enter Your Option");
+			System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+			option = scanner.next().charAt(0);
+			System.out.println("\n");
+			
+			switch (option) {
+			
+			case 'A':
+				System.out.println("-------------------------------------------------------");
+				System.out.println("Balance = "+balance);
+				System.out.println("-------------------------------------------------------");
+				System.out.println("\n");
+				break;
+				
+			case 'B':
+				System.out.println("-------------------------------------------------------");
+				System.out.println("Enter an amount to deposit ");
+				System.out.println("-------------------------------------------------------");
+				
+				int amount = scanner.nextInt();
+				deposit(amount);
+				System.out.println("\n");
+				break;
+				
+			case 'C':
+				System.out.println("-------------------------------------------------------");
+				System.out.println("Enter an amount to withdraw ");
+				System.out.println("-------------------------------------------------------");
+				
+				int amount2 = scanner.nextInt();
+				withdraw(amount2);
+				System.out.println("\n");
+				break;
+				
+			case 'D':
+				System.out.println("-------------------------------------------------------");
+				getPreviousTransaction();
+				System.out.println("-------------------------------------------------------");
+				System.out.println("\n");
+				break;
+				
+			case 'E' :
+				System.out.println("=========================================================================================================");
+				break;
+
+			default:
+				System.out.println("Invalid Option!! Please Enter Correct Opton...");
+				break;
+			}			
+		}
+		while(option != 'E');
+			System.out.println("Thank You for Using our Services.....!!");
+	}
         
     }
     
