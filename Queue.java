a40da10ca4a411f8f96606b7f074a9d782cb0b02
Ghassan El Turk <gass304@gmail.com>
2022-12-02 15:21:44 +0200

    Adding queue data structure and implementing is full method

---
+++ Queue.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking.system.project;
+
+/**
+ *
+ * @author GASS
+ */
+public class Queue {
+     int SIZE = 5;
+  int items[] = new int[SIZE];
+  int front, rear;
+  
+   Queue() {
+    front = -1;
+    rear = -1;
+  }
+    boolean isFull() {
+    if (front == 0 && rear == SIZE - 1) {
+      return true;
+    }
+    return false;
+  }
+    
+}
