5f9396dd40552a9ef644b4e491e6c670242cacbd
Ghassan El Turk <gass304@gmail.com>
2022-12-02 14:37:29 +0200

    Creating display method that displays the elements in the stack

--- Stack.java
+++ Stack.java
@@ -32,12 +32,49 @@ public class Stack {
             transactionLogs[currentNumber] = null;
             return temp;
         }
+         public void expand(){
+            if(isFull()) {
+                int size = 20 + transactionLogs.length;
+                Transaction [] newarray = new Transaction [size];
+                this.transactionLogs = reverseStack(transactionLogs);
+                //store currentNb in a temp
+                int temp = this.currentNumber;
+                for(int i = 0; i<transactionLogs.length;i++){
+                    newarray[i] = Pop();
+                }
+                transactionLogs = newarray;
+                currentNumber = temp;
+            }
+
+        }
          public void Push(Transaction transaction){
             if(isFull()){
                 expand();
             }
             transactionLogs[currentNumber] = transaction;
             currentNumber++;
+        }
+          public void display(){
+           
+            int size = currentNumber;
+            Transaction [] tempStack = new Transaction [size];
+            
+            for(int i = 0; i < size; i++){
+                tempStack[i] = this.Pop();
+            }
+            
+            Transaction temp;
+            int nbOfElements = tempStack.length-1;
+            for (int j = 0; j < tempStack.length ; j++){
+                //Pop:
+                temp = tempStack[nbOfElements];
+                tempStack[nbOfElements] = null;
+                nbOfElements--;
+                //Print:
+                System.out.println("Element at ["+j+"] = "+temp.toString());
+                //Push:
+                this.Push(temp);
+            }
         }
     
 }
